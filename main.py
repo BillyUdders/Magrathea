6c7d16f7b496a7c54d7356cdf2753dab5353c230
Jon Church <Jrchurch02@gmail.com>
2025-12-04 22:49:32 -0500

    aaaaaaaa

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def main():
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3, 4],[1, 4, 3, 2])
+    plt.show()
     print("Hello from Magrathea! bill homo")
 
 
