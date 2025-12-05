639b26f20b405c6cea33f8319520d94b4920ed1a
Rhys Davies <rhys.davies901@gmail.com>
2025-12-04 19:59:42 -0800

    blah fuck

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-def main():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4],[1, 4, 3, 2])
+
+def show_map():
+    _, ax = plt.subplots()
+    ax.plot([1, 2, 3, 4], [1, 4, 3, 2])
     plt.show()
-    print("Hello from Magrathea! bill homo")
 
 
 if __name__ == "__main__":
-    main()
+    show_map()
