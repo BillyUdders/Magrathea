abefa7addda813021689d5a85c579ab196264654
Rhys Davies <rhys.davies901@gmail.com>
2025-12-04 20:14:09 -0800

    feature(noise): Basical perlin-noise function

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from perlin_noise import PerlinNoise
+
+
+def generate_heightmap(size, octaves):
+    noise = PerlinNoise(octaves=octaves)
+    data = np.zeros((size, size))
+
+    for y in range(size):
+        for x in range(size):
+            nx = x / size
+            ny = y / size
+            data[y][x] = noise([nx, ny])  # Perlin noise value
+
+    return data
 
 
 def show_map():
-    _, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4], [1, 4, 3, 2])
+    heightmap = generate_heightmap(2048, 4)
+
+    plt.figure(figsize=(6, 6))
+
+    plt.title("Perlin Noise Heightmap")
+    plt.imshow(heightmap, cmap="terrain", interpolation="nearest")
+    plt.colorbar(label="Height")
+    plt.axis("off")
+
     plt.show()
 
 
