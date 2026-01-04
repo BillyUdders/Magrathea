d20f0412971d9b548b7a2500821d52e3f5de0296
Rhys Davies <rhys.davies901@gmail.com>
2026-01-03 18:44:35 -0800

    chore(forgot to add): files

---
+++ tests/test_rendering_engine.py
@@ -0,0 +1,18 @@
+import os
+from magrathea.rendering_engine import render_map_to_buffer, render_map_to_png
+
+
+def test_render_png(tmp_path):
+    d = tmp_path / "output"
+    d.mkdir()
+    filename = d / "test_output.png"
+
+    render_map_to_png(64, 2, str(filename))
+
+    assert os.path.exists(filename), "PNG file should be created"
+
+
+def test_render_buffer():
+    buf = render_map_to_buffer(64, 2)
+    assert buf.getbuffer().nbytes > 0, "Buffer should contain data"
+    buf.close()
