29380c815b596a1e1b90b7ca0a35be44d2ee639e
Rhys Davies <rhys.davies901@gmail.com>
2026-01-03 17:38:58 -0800

    refactor: extract plot_map to DRY up visualization logic

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ def generate_heightmap(size, octaves):
     return data
 
 
-def show_map():
-    heightmap = generate_heightmap(128, 4)
-
+def plot_map(heightmap):
     plt.figure(figsize=(6, 6))
 
     plt.title("Perlin Noise Heightmap")
@@ -26,19 +24,16 @@ def show_map():
     plt.colorbar(label="Height")
     plt.axis("off")
 
+
+def show_map():
+    heightmap = generate_heightmap(128, 4)
+    plot_map(heightmap)
     plt.show()
 
 
 def render_map_to_png(size, octaves, filename):
     heightmap = generate_heightmap(size, octaves)
-
-    plt.figure(figsize=(6, 6))
-
-    plt.title("Perlin Noise Heightmap")
-    plt.imshow(heightmap, cmap="terrain", interpolation="nearest")
-    plt.colorbar(label="Height")
-    plt.axis("off")
-
+    plot_map(heightmap)
     plt.savefig(filename)
     plt.close()
 
